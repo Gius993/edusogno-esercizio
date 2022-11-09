913d6533856fab6cd2f36b9e06238152f5b181ae
Gius993 <giuseppevidili9@gmail.com>
2022-11-09 17:59:35 +0100

    validazione password e login'da debuggare'

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+$email = $conn->real_escape_string($_POST['email']);
+$password = $conn->real_escape_string($_POST['password']);
+
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+	$sql_select = "SELECT * FROM utenti WHERE email = $email";
+	if($result = $conn->query($sql_select)){
+		if($result->num_rows == 1){
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+			if(password_verify($password, $row['password'])){
+				session_start();
+				$_SESSION['loggato'] = true;
+				$_SESSION['id'] = $row['id'];
+				$_SESSION['email'] = $row['email'];
+
+				header('location: area-privata.php');
+
+			}else{
+				echo ' la password non corriponde';
+			}
+		}
+	}
+	$conn->close();
+}
+
+?>
--- register.php
+++ register.php
@@ -5,8 +5,10 @@ $nome = $conn->real_escape_string($_POST['nome']);
 $cognome = $conn->real_escape_string($_POST['cognome']);
 $email = $conn->real_escape_string($_POST['email']);
 $password = $conn->real_escape_string($_POST['password']);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$password')";
+
+$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$hashed_password')";
 if($conn->query($sql) === true){
 	echo 'Registrazione effetuata';
 }else{
